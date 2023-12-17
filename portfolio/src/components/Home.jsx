47461072975a31ee43979c1ece5011d64cd6f857
Yash Kumar Singh Patwa <yashkumarsinghpatwa7889@gmail.com>
2023-12-17 13:31:31 +0530

    Initial Commit

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons';
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#0a192f]'>
+    </div>
+  )
+}
+
+export default Home
